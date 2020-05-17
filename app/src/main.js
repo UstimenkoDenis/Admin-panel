ba7a632413df653bfddfdd91f2648bd9e9bca84a
Denis Ustimenko <ustimenkodenis@mail.ru>
2020-05-17 20:40:56 +0300

    FirsAPI added

--- app/src/main.js
+++ app/src/main.js
@@ -1,24 +1,5 @@
-import $ from 'jquery'; //Когда  мы используем такой импорт - нам нужна система сбоки в один рабочий файл
-$('h1').fadeOut(); //команда будет убирать наш заголовок , но сейчас будет ошибка тк не стоит система сборки
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './components/editor';
 
-
-// Приступим к настройке системы сборки нашего проекта
-// Наши JS файлы мы будем компилировать при помощи webpack, однако - Все задачи мы будем запускать при помощи GULP 
-
-//////WEBPACK
-// Установка webpack:  npm i --save-dev webpack webpack-cli webpack-stream -
-
-//Первый - сам вебпэк
-// второй -  чтобы мы могли давать задания из клиента
-// последний чтобы все успешно обрабатывалось в gulp 
-
-
-/// BABEL
-// Установка babel npm i @babel/core @babel/preset-env @babel/preset-react babel-loader --save-dev
-
-///// ПОЛИФИЛЫ
-// Добавим еще библиотеку содержащую полифилы:     npm i core-js --save
-
-///scss
-
-//npm i gulp-sass --save-dev
+ReactDOM.render(<Editor/> , document.getElementById('root'));
--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,70 @@
-const gulp = require('gulp');
-const webpack = require('webpack-stream');
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const sass = require("gulp-sass");
 
-const dist = 'C:/openServer/OSPanel/domains/localhost/react_admin/admin'
-// 1 задача - чтобы index.html копировался на сервер
+const dist = "/MAMP/htdocs/react_admin/admin";
 
-gulp.task("copy-html",()=>{
-    return gulp.src('./app/src/index.html')
-        .pipe(gulp.dest(dist))
-     
-})
-// чтобы запустить задачу пишем gulp copy-html
-// gulp скопирует и теперь мы можем в браузере зайти http://localhost/react_admin/admin/ и там будет наш html !
-
-// jS будем формировать при помощи webpack
-
-gulp.task("build-js",()=>{
-    return gulp.src('./app/src/main.js')
-        .pipe(webpack({
-                mode: 'development',
-                output: {
-                    filename: 'script.js'
-                },
-                watch: false,
-                devtool: 'source-map',
-                ////////////////////////////////////////////// этот код вставлен с сайта webpack
-                module: {
-                  rules: [
-                    {
-                      test: /\.m?js$/,
-                      exclude: /(node_modules|bower_components)/,
-                      use: {
-                        loader: 'babel-loader',
-                        options: {
-                          presets:[['@babel/preset-env',{     // запишем настройки для пресета
-                                debug: true,
-                                corejs: 3,
-                                useBuiltIns: "usage"
-
-                                // далее в package.json допишем "browserslist": [
-//                                  "last 2 chrome versions",
-//                                  "last 2 firefox versions",
-//                                  "last 2 ios versions",
-                             //   ]
-                          }],
-                          "@babel/react"]
-                        }
-                      }
-                    }
-                  ]
-                }
-             ////////////////////////////////////////////// этот код вставлен с сайта webpack
-        }))
-        .pipe(gulp.dest(dist))
-})
-
-
-// Запусить задучу :
-// gulp copy-html
-// gulp build-js
-
-gulp.task('build-sass', () => {
-  return gulp.src('./app/scss/style.scss')
-          .pipe(sass().on('error', sass.logError))
-          .pipe(gulp.dest(dist))
-})
-
-gulp.task('copy-api', () => {
-  return gulp.src('./app/api/**/*.*')
-            .pipe(gulp.dest(dist+'/api'))
-})
-
-gulp.task('copy-assets', () => {
-  return gulp.src('./app/assets/**/*.*')
-           .pipe(gulp.dest(dist + '/assets'))
-})
-
-// gulp сдеди за изменениями:
-// за чем следим, какая задача
-gulp.task('watch', () => {
-  return gulp.watch('./app/src/index.html', gulp.parallel('copy-html'));
-         gulp.watch('./app/assets/**/*.*', gulp.parallel('copy-assets'));
-         gulp.watch('./app/api/**/*.*', gulp.parallel('copy-api'));
-         gulp.watch('./app/scss/**/*.scss', gulp.parallel('build-sass'));
-         gulp.watch('./src/**/*.*', gulp.parallel('build-js'));
-         
+gulp.task("copy-html", () => {
+    return gulp.src("./app/src/index.html")
+                .pipe(gulp.dest(dist));
 });
-// Важно watch - отслеживает только последующие изменения после запуска этой команды
 
-// Поэтому сделаем таск билд чтобы полностью сбилдить наш проект - будет запускать не только те задачи которые нужны
-// в данный момент а сразу все
+gulp.task("build-js", () => {
+    return gulp.src("./app/src/main.js")
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    watch: false,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }],
+                                 "@babel/react"]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist));
+});
 
-gulp.task('build', gulp.parallel('copy-html', 'copy-assets', 'copy-api', 'build-sass', 'build-js'));
+gulp.task("build-sass", () => {
+    return gulp.src("./app/scss/style.scss")
+                .pipe(sass().on('error', sass.logError))
+                .pipe(gulp.dest(dist));
+});
 
-// чтобы запускать все одной командой gulp :
+gulp.task("copy-api", () => {
+    return gulp.src("./app/api/**/*.*")
+                .pipe(gulp.dest(dist + "/api"));
+});
 
-gulp.task('default', gulp.parallel('watch', 'build')); 
+gulp.task("copy-assets", () => {
+    return gulp.src("./app/assets/**/*.*")
+                .pipe(gulp.dest(dist + "/assets"));
+});
+
+gulp.task("watch", () => {
+    gulp.watch("./app/src/index.html", gulp.parallel("copy-html"));
+    gulp.watch("./app/assets/**/*.*", gulp.parallel("copy-assets"));
+    gulp.watch("./app/api/**/*.*", gulp.parallel("copy-api"));
+    gulp.watch("./app/scss/**/*.scss", gulp.parallel("build-sass"));
+    gulp.watch("./app/src/**/*.js", gulp.parallel("build-js"));
+});
+
+gulp.task("build", gulp.parallel("copy-html", "copy-assets", "copy-api", "build-sass", "build-js"));
+
+gulp.task("default", gulp.parallel("watch", "build"));
