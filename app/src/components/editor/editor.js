71b8fded8146fc5a4c80c7a1ec6a07012a3546d4
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-06-18 23:55:55 +0300

    optimization

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -2,6 +2,7 @@ import '../../helpers/iframeLoader.js';
 import axios from 'axios';
 import React, {Component} from 'react';
 import DOMHelper from '../../helpers/dom-helper';
+import EditorText from '../editor-text';
 
 export default class Editor extends Component {
     constructor() {
@@ -39,8 +40,7 @@ export default class Editor extends Component {
             .then(html => axios.post("./api/saveTempPage.php", {html}))
             .then(() => this.iframe.load("../temp.html"))
             .then(() => this.enableEditing()) 
-   
-                  
+            .then(()=> this.injectStyles())                 
     }
 
     save() {
@@ -53,16 +53,26 @@ export default class Editor extends Component {
 
     enableEditing() {
         this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach( element => {
-            element.contentEditable = "true";                 // ставим ему свойство редактирования
-            element.addEventListener("input", () => {
-                this.onTextEdit(element);  // этот метод отвечает за синхронизацию всех изменений
-            });
+            const id = element.getAttribute("nodeid");
+            const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
+
+            new EditorText(element, virtualElement);
         });        
     }
 
-    onTextEdit(element) {
-        const id = element.getAttribute("nodeid")
-        this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
+    injectStyles() {
+        const style = this.iframe.contentDocument.createElement("style");
+        style.innerHTML = `        
+            text-editor:hover {
+                outline: 3px solid orange;
+                outline-offset: 8px;
+            }
+            text-editor:focus {
+                outline: 3px solid red;
+                outline-offset: 8px;
+            }
+        `;
+        this.iframe.contentDocument.head.appendChild(style);
     }
 
     loadPageList() {
@@ -100,8 +110,7 @@ export default class Editor extends Component {
         return (  
             <>     
                 <button onClick = {() => this.save()}>Click</button>    
-                <iframe src = {this.currentPage} frameBorder = "0"></iframe>
-                 
+                <iframe src = {this.currentPage} frameBorder = "0"></iframe>                 
             </>
 
             // <>
